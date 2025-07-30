0e995ee16d613b84862eb1184983975f44b4312d
Axel Martin <axel.martin@outlook.com>
2025-07-30 15:27:26 +0200

    chore: configure next-intl plugin in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
 
+/*-------------------------------------------------*
+//* Configuration personnalisée de Next.js
+*--------------------------------------------------*/
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Otionnel : pour utiliser aussi le support i18n natif de Next.js :
+  i18n: {
+    locales: ['fr', 'en', 'de', 'es'],
+    defaultLocale: 'fr',
+  },
 };
 
-export default nextConfig;
+/*-------------------------------------------------*
+// * On enveloppe la config avec le plugin next-intl :
+// - Le plugin génère les alias nécessaires et actie le chargement des messages selon la locale
+*--------------------------------------------------*/
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(nextConfig);
