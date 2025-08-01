5b75d849d50d4e6f87ef92477f6ee054afb80183
Axel Martin <axel.martin@outlook.com>
2025-08-01 15:59:47 +0200

    chore: configure next-intl plugin in next.config.ts

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import styles from './page.module.scss';
 
 export default function About() {
-  return <div>About</div>;
+  return <div className={styles.about}>About</div>;
 }
