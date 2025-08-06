0131a0aab646bc6936a4f58b75545b25ea56518d
Axel Martin <axel.martin@outlook.com>
2025-08-06 17:25:33 +0200

    feat: created header structure for desktop version

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -3,22 +3,6 @@ import styles from './Header.module.scss';
 
 export default function Header({ locale }: { locale: string }) {
   return (
-    // <header className={styles.headerRibbon}>
-    //   <div className={styles.header}>
-    //     <div className={styles.logoContainer}>
-    //       <Link
-    //         href="/"
-    //         className={styles.logoLink}
-    //         aria-label="go to homepage"
-    //       >
-    //         <div className={styles.logo}></div>
-    //       </Link>
-    //     </div>
-
-    //     <div className={styles.burgerMenu}></div>
-    //   </div>
-    // </header>
-
     <header className={styles.headerRibbon}>
       <div className={styles.header}>
         <div className={styles.logoContainer}>
@@ -33,7 +17,17 @@ export default function Header({ locale }: { locale: string }) {
 
         <div className={styles.burgerMenu}></div>
 
+        <div className={styles.navContainer}>
+          <div className={styles.navBar}>
+            <div className={styles.link}>Lien 1</div>
+            <div className={styles.link}>Accueil</div>
+            <div className={styles.link}>FAQ</div>
+            <div className={styles.link}>Lien 1</div>
+          </div>
+        </div>
+
         <div className={styles.optionsContainer}>
+          <div className={styles.ctaBtn}>Rencontrons-nous</div>
           <div className={styles.localeSwitcherContainer}>
             <div className={styles.localeSwitcher}></div>
           </div>
